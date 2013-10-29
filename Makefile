1cdfeb3e9ac9884de8485c8a73532c5c311dcd7f
Aleksi Sapon <QcTechs@gmail.com>
2013-10-28 20:26:45 -0400

    Use color thresholding to find red in the video from the webcam
    Just starting to mess around with things
    Also, fix the makefile; you need pkg-config to compile the project

--- Makefile
+++ Makefile
@@ -1,44 +1,21 @@
 COMPILER=G++
-
-# todo: object files into output path, processing c / c++ files in the same time (?), nested directories for source files (?)
 C = cpp
 OUTPUT_PATH = bin/
 SOURCE_PATH = src/
 EXE = $(OUTPUT_PATH)CV-Buoy
 MKDIR_P = mkdir -p
-
-ifeq ($(COMPILER), G++)
-  ifeq ($(OS),Windows_NT)
-    OBJ = obj
-  else
-    OBJ = o
-  endif
-  COPT = -O2
-  CCMD = g++
-  OBJFLAG = -o
-  EXEFLAG = -o
-# INCLUDES = -I../.includes
-  INCLUDES =
-# LIBS = -lgc
-  LIBS =
-# LIBPATH = -L../gc/.libs
-  LIBPATH =
-  CPPFLAGS = $(COPT) -g $(INCLUDES)
-  LDFLAGS = $(LIBPATH) -g $(LIBS)
-  DEP = dep
-else
-  OBJ = obj
-  COPT = /O2
-  CCMD = cl
-  OBJFLAG = /Fo
-  EXEFLAG = /Fe
-# INCLUDES = /I..\\.includes
-  INCLUDES =
-# LIBS = ..\\.libs\\libgc.lib
-  LIBS =
-  CPPFLAGS = $(COPT) /DEBUG $(INCLUDES)
-  LDFLAGS = /DEBUG
-endif
+OBJ = o
+COPT = -O2
+CCMD = g++
+OBJFLAG = -o
+EXEFLAG = -o
+INCLUDES =
+LIBS = `pkg-config --libs opencv`
+LIBPATH =
+CFLAGS = `pkg-config --cflags opencv`
+CPPFLAGS = $(CFLAGS) $(COPT) -g $(INCLUDES)
+LDFLAGS = $(LIBPATH) -g $(LIBS)
+DEP = dep
 
 OBJS := $(patsubst %.$(C),%.$(OBJ),$(wildcard $(SOURCE_PATH)*.$(C)))
 
@@ -55,4 +32,3 @@ clean:
 	rm -rf $(SOURCE_PATH)*.$(OBJ) $(EXE)
 
 rebuild: clean all
-#rebuild is not entirely correct
--- src/BuoyFinder.cpp
+++ src/BuoyFinder.cpp
@@ -1,11 +1,70 @@
-#include "BuoyFinder.h"
-
-#include <cstdio>
 #include <iostream>
+#include <cv.h>
+#include <highgui.h>
+
+using namespace std;
+using namespace cv;
+
+// The lower bound of the threshold (currently a shade of red)
+#define LOW_COLOR 170, 160, 70
+// The higher bound of the threshold (currently a shade of red)
+#define HIGH_COLOR 180, 256, 256
+
+// Gets the black and white thresholded image from the original color image
+static IplImage* getThresholdImage(IplImage* image) {
+	// Smooth to reduce noise
+	cvSmooth(image, image, CV_GAUSSIAN, 3, 3);
+	// Convert from RGB to HSV
+	IplImage* HSVImage = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 3);
+	cvCvtColor(image, HSVImage, CV_BGR2HSV);    
+	// Create the threshold image
+	IplImage* thresholdImage = cvCreateImage(cvGetSize(HSVImage), IPL_DEPTH_8U, 1);
+	cvInRangeS(HSVImage, cvScalar(LOW_COLOR), cvScalar(HIGH_COLOR), thresholdImage);
+	// Release the temporary HSV image
+	cvReleaseImage(&HSVImage);
+	// Return the threshold image
+	return thresholdImage;
+} 
 
 int main(int argc,char *argv[]) {
-
-	std::cout << "This is a template!" << std::endl;
-
+	// Get the first camera, fail if it's not available
+	CvCapture* camera = cvCaptureFromCAM(0);
+	if (camera == NULL) {
+		cout << "No available camera!" << endl;
+		return 1;
+	}
+	// Get the horizontal resolution (width) of the image to place the windows correctly
+	double horizontalResolution = cvGetCaptureProperty(camera, CV_CAP_PROP_FRAME_WIDTH);
+	// Create and place the windows
+	cvNamedWindow("Input");
+	cvMoveWindow("Input", 100, 100);
+	cvNamedWindow("Output");
+	cvMoveWindow("Output", 110 + int(horizontalResolution), 100);
+	// Get the first frame from the camera
+	IplImage* frame = cvQueryFrame(camera);
+	// As long as we have a frame
+	while (frame != NULL) {
+		// Clone the frame, we can't work of the captured one
+		frame = cvCloneImage(frame); 
+		// Threshold it
+		IplImage* thresholdImage = getThresholdImage(frame);
+		// Show the original image and the thresholded one
+		cvShowImage("Input", frame);
+		cvShowImage("Output", thresholdImage);
+		// Release the thresholded image
+		cvReleaseImage(&thresholdImage);
+		// Wait 50ms for the escape key
+		if (cvWaitKey(10) == 27) {
+			// If escape was pressed, exit
+			break;
+		}
+		// Query the next frame
+		frame = cvQueryFrame(camera);
+	}
+	// Release the frame (the clone), the windows and the camera
+	cvReleaseImage(&frame);
+	cvDestroyAllWindows();
+	cvReleaseCapture(&camera);
+	// Return successfully
 	return 0;
 }
